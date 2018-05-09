6499a8ebcfe95e3a32bfb39ef31bc616ef613efe
Irina <irina.rozhnovskay@gmail.com>
2018-05-10 00:35:20 +0300

    SpringSecurity

---
+++ src/main/java/com/starter/Application.java
@@ -0,0 +1,26 @@
+package com.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by Yuksi on 14.09.2017.
+ */
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+
+
+}
